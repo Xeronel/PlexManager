b61285293d6550bfecfaba023f125cc4d0ad5173
JWelsh97 <jackwelsh@protonmail.com>
2017-05-01 15:50:49 +0000

    Added TV and Movie lookups to request page

--- web/static/requests.js
+++ web/static/requests.js
@@ -1,38 +1,89 @@
 var requests = {};
 requests.last_search_time = Date.now();
 requests.last_search = '';
+requests.search_func = requests.search_show;
 
-requests.search_show = function (e) {
+requests.add_show = function(obj) {
+    var output = $('#output');
+    output.append(
+        '<div class="row content-data">' +
+        '<img id="poster" src="' + obj.show.image.medium + '" class="col-lg-4 col-md-4 col-sm-4 col-xs-4">' +
+        '<div class="info col-lg-8 col-md-8 col-sm-8 col-xs-8"><a id="name" href="' + obj.show.url + 'class="name">' + obj.show.name + '</a>' +
+        '<span class="row description col-lg-12 col-md-12 col-sm-12 col-xs-12">' + obj.show.summary + '</span></div>' +
+        '</div><hr width="95%">'
+    )
+};
+
+requests.search_show = function (show_name) {
+  // Get showw
+  $.get('http://api.tvmaze.com/search/shows?q=' + show_name, function (data) {
+      var output = $('#output')
+      // Clear output div
+      output.html('');
+
+      // Add each show to output
+      $(data).each(function (idx, obj) {
+          if (obj.show.image) {
+              requests.add_show(obj);
+          }
+      });
+  });
+}
+
+requests.add_movie = function(obj) {
+    $('#output').append(
+        '<div class="row content-data">' +
+        '<img id="poster" src="https://image.tmdb.org/t/p/w150/' + obj.poster_path + '" class="col-lg-4 col-md-4 col-sm-4 col-xs-4">' +
+        '<div class="info col-lg-8 col-md-8 col-sm-8 col-xs-8"><a id="name" href="https://www.themoviedb.org/movie/' + obj.id + 'class="name">' + obj.title + '</a>' +
+        '<span class="row description col-lg-12 col-md-12 col-sm-12 col-xs-12">' + obj.overview + '</span></div>' +
+        '</div><hr width="95%">'
+    )
+};
+
+requests.search_movie = function(movie_name) {
+  $.get('https://api.themoviedb.org/3/search/movie?api_key=146285a857bd0e2e19974450468dd5d7&query=' + movie_name, function (data) {
+      $('#output').html('');
+      $(data.results).each(function (idx, obj) {
+          if (obj.poster_path) {
+              requests.add_movie(obj);
+          }
+      });
+  });
+}
+
+requests.search = function (e) {
     if (Date.now() - requests.last_search_time > 500
         && e.value !== requests.last_search
         && e.value !== '') {
-        var value = e.value;
-        $.get('http://api.tvmaze.com/search/shows?q=' + value, function (data) {
-            var output = $('#output');
-            output.html('');
-            $(data).each(function (idx, obj) {
-                if (obj.show.image) {
-                    output.append('<img src="' + obj.show.image.medium + '">');
-                    output.append(obj.show.name + '<br>');
-                }
-            });
-        });
-        requests.last_search = value;
+        requests.last_search = e.value;
         requests.last_search_time = Date.now();
+        requests.search_func(e.value);
         return true;
     } else {
         setTimeout(function () {
-            requests.search_show(e);
+            requests.search_func(e.value);
         }, 500);
         return false;
     }
 };
 
-$(function () {
-    $('#search').keyup(function () {
-        var result = requests.search_show(this);
-        if (result) {
-            console.log("Fired late.");
-        }
-    });
+$('#tv_button').click(function () {
+    $('#tv_button').addClass('active');
+    $('#movies_button').removeClass('active');
+    requests.search_func = requests.search_show;
+    requests.search({value: $('#search').val()});
+});
+
+$('#movies_button').click(function () {
+    $('#movies_button').addClass('active');
+    $('#tv_button').removeClass('active');
+    requests.search_func = requests.search_movie;
+    requests.search({value: $('#search').val()});
+});
+
+$('#search').keyup(function () {
+  var result = requests.search(this);
+  if (result) {
+    console.log("Fired late.");
+  }
 });
