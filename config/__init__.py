5aadc84dc78893aee3459bcc69b11cfad25d1fed
Ripster <ripster1121@gmail.com>
2017-05-02 13:04:23 -0400

    Handle missing config sections

--- config/__init__.py
+++ config/__init__.py
@@ -10,6 +10,6 @@ file_name = __os.path.join(base, 'config.yaml')
 with open(file_name, 'r') as f:
     __raw_config = __yaml.load(f)
 
-db = __DBConfig(__raw_config['db'])
-web = __WebConfig(__raw_config['web'])
+db = __DBConfig(__raw_config.get('db', {}))
+web = __WebConfig(__raw_config.get('web', {}))
 __all__ = ['db', 'web']
